47d77be343f1d992db0df96114dba0e576b5303f
SamuelHenrique32 <shdalmas@ucs.br>
2019-01-29 23:40:36 -0200

    Aula 25 - Passando parametros a controladores

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::get('/', function () {
 
 //ir para controlador e chama getNome
 Route::get('/nome', 'MeuControlador@getNome');
+
+Route::get('/idade', 'MeuControlador@getIdade');
+
+//parametros para controlador
+Route::get('/multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
+
+Route::get('/nomes/{id}', 'MeuControlador@getNomeByID');
